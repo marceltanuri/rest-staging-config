7d3885d988f1cd2fe30261c50c1e05916e470e5d
Marcel Tanuri <marcel.tanuri@gmail.com>
2019-05-15 15:28:11 -0300

    update Authentication class

--- src/main/java/br/com/mtanuri/liferay/rest/stagingconfig/Authentication.java
+++ src/main/java/br/com/mtanuri/liferay/rest/stagingconfig/Authentication.java
@@ -7,16 +7,14 @@ import org.springframework.stereotype.Component;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.model.Group;
-import com.liferay.portal.model.Role;
-import com.liferay.portal.model.User;
+import com.liferay.portal.model.RoleConstants;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
 import com.liferay.portal.service.GroupLocalServiceUtil;
+import com.liferay.portal.service.RoleLocalServiceUtil;
 import com.liferay.portal.service.UserLocalServiceUtil;
 
 @Component
 public class Authentication {
-
-	private static final String ADMINISTRATOR_ROLE = "Administrator";
 
 	private static Log _log = LogFactoryUtil.getLog(Authentication.class);
 
@@ -30,22 +28,19 @@ public class Authentication {
 				|| !groupId.isEmpty()) {
 
 			try {
-				
+
 				Group group = GroupLocalServiceUtil.getGroup(Long.valueOf(groupId));
 				String authType = CompanyLocalServiceUtil.getCompany(group.getCompanyId()).getAuthType();
 				long authenticatedUser = UserLocalServiceUtil.authenticateForBasic(group.getCompanyId(), authType,
 						username, password);
-				
+
 				if (authenticatedUser > 0) {
-					User user = UserLocalServiceUtil.getUser(authenticatedUser);
-					for (Role role : user.getRoles()) {
-						
-						if (role.getType() == 1 && role.getName().equals(ADMINISTRATOR_ROLE)) {
-							return true;
-						}
+					if (RoleLocalServiceUtil.hasUserRole(authenticatedUser, group.getCompanyId(),
+							RoleConstants.ADMINISTRATOR, true)) {
+						return true;
 					}
 				}
-				
+
 			} catch (Exception e) {
 				_log.info("Authentication error: " + e.getMessage());
 				return false;
